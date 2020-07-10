546ee59f0fd53540a7b677e8f6537d87d243f34b
Manlio <manlio92--@live.it>
2020-07-10 23:23:56 +0100

    Single Post Display

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 import PrivateRoute from './components/routing/PrivateRoute';
 import './App.css';
 // Redux
@@ -63,6 +64,7 @@ const App = () => {
                 path='/add-education'
                 component={AddEducation}
               />
+              <PrivateRoute exact path='/posts/:id' component={Post} />
               <PrivateRoute exact path='/posts' component={Posts} />
             </Switch>
           </section>
